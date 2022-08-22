813aecca91bc530e0495a40e83565c40acb8c64b
Jack Wilkinson <me@jackwilky.com>
2022-08-22 04:52:46 +0100

    Added config options

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 return [
     'stream_s3_uploads' => [
         /**
@@ -10,6 +12,16 @@ return [
         /**
          * This tells the signed url generator to ignore custom AWS_URL values
          */
-        'ignore_aws_url' => env('STREAM_IGNORE_AWS_URL', false)
+        'ignore_aws_url' => env('STREAM_S3_IGNORE_AWS_URL', false),
+
+        /**
+         * The expatriation time for the url in minutes
+         */
+        'url_expires_after' => env('STREAM_S3_URL_EXPIRES_AFTER', 5),
+
+        /**
+         * The maximum size in bytes for file uploads
+         */
+        'max_upload_size' => env('STREAM_S3_MAX_UPLOAD_SIZE', UploadedFile::getMaxFilesize())
     ],
 ];
